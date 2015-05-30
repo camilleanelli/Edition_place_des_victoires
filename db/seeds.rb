071222a3047ba6d0bef889797ace49dd97d9df86
julien michot <ju.michot@gmail.com>
2015-05-30 15:16:18 +0200

    Add anonymous data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
-author1 = Author.create!(firstname: "Gilles", lastname: "Bastianelli", bio: "Biographe, documentariste, photographe")
-author2 = Author.create!(firstname: "Galina", lastname: "Manevich")
+# Needed for /admin
+# AdminUser.create!(email: "admin@example.com", password: "camcamcam", password_confirmation: "camcamcam")
 
+def create_author
+  Author.create!(firstname: Faker::Name::first_name, lastname: Faker::Name.last_name, bio: Faker::Lorem.paragraph)
+end
 
-book1 = Book.create!(title: "Edik Steinberg", size: "240mm x 300mm, 40mm dos", page_count: 432, weight: 2800, cover_type: "Relié sous jaquette", isbn: "9782809912722", illustration_count: 400, price: 4995, description: "Cette monographie est la plus ambitieuse jamais réalisée sur cet immense peintre russe contemporain.  Depuis sa disparition en 2012 à Paris, il a été exposé dans les musées Pouchkine à Moscou, Ermitage de Saint-Pétersbourg et Wiesbaden en Allemagne.  Son oeuvre géométrique est un merveilleux voyage initiatique dans la Russie que l'on aime pour sa culture, sa spiritualité, son histoire et sa beauté.  Cet ouvrage marque aussi le centenaire du Carré Noir de Malevitch (1915) dont Steinberg a ranimé le suprématisme dans les années 1970.")
+def create_book
+  book = Book.create!(title: Faker::Commerce.product_name, size: "240mm x 300mm, 40mm dos", page_count: Faker::Number.number(3), weight: Faker::Number.number(4), cover_type: "Relié sous jaquette", isbn: Faker::Code.isbn, illustration_count: Faker::Number.number(3), price: Faker::Number.number(3), description: Faker::Lorem.paragraph(2))
+  rand(1..3).times { book.authors << create_author }
+end
 
-book1.authors << author1
-book1.authors << author2
-
-
-AdminUser.create!(email: "admin@example.com", password: "camcamcam", password_confirmation: "camcamcam")
+100.times do
+  create_book
+end
