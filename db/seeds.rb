ceae962f9cc71f1dc1113d4af746794c4573270e
camilleanelli <camilleanelli32@gmail.com>
2015-06-03 17:35:28 +0200

    add category id

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@ def create_author
 end
 
 def create_book
-  book = Book.create!(title: Faker::Commerce.product_name, size: "240mm x 300mm, 40mm dos", page_count: Faker::Number.number(3), weight: Faker::Number.number(4), cover_type: "Relié sous jaquette", isbn: Faker::Code.isbn, illustration_count: Faker::Number.number(3), price: Faker::Number.number(3), description: Faker::Lorem.paragraph(2))
+  book = Book.create!(title: Faker::Commerce.product_name, category_id: Faker::Number.digit, size: "240mm x 300mm, 40mm dos", page_count: Faker::Number.number(3), weight: Faker::Number.number(4), cover_type: "Relié sous jaquette", isbn: Faker::Code.isbn, illustration_count: Faker::Number.number(3), price: Faker::Number.number(3), description: Faker::Lorem.paragraph(2))
   rand(1..3).times { book.authors << create_author }
 end
 
+def create_category
+  category = Category.create!(name: Faker::Name.name)
+end
+
+ 
+
 100.times do
   create_book
+  create_category
 end
